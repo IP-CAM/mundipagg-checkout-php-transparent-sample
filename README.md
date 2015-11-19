43425853c1cbd53ace027d67a9a688521147b71f
cristiano-acosta <cristiano-acosta@hotmail.com>
2015-11-19 02:53:14 -0200

    Comecando os trabalhos
    Apenas testando algumas coisas para habilitar o pagamento

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# MundiPagg Checkout Transparent Sample
+# MundiPagg Opencart Checkout Transparent Sample
 
-```
-$ git clone git@github.com:mundipagg/mundipagg-checkout-php-transparent-sample.git
-$ composer install
-```
+Baseado no [MundiPagg Checkout Transparent Sample](https://github.com/mundipagg/mundipagg-checkout-php-transparent-sample "MundiPagg Opencart")  apenas um simples modulo para integração com Opencart 2.0.x
+
+**Em breve para: Opencart 2.1.0.1**
+
+
+![checkout](http://i.imgur.com/ejo1UvI.jpg)
+
+![checkout](http://i.imgur.com/ma9O1W7.jpg)
+
+![checkout](http://i.imgur.com/Jw8krhe.jpg)
 
 ![checkout](http://i.imgur.com/jNjkX9I.png)
+
---
+++ upload/catalog/controller/payment/mundipagg.php
@@ -0,0 +1,58 @@
+<?php
+  class  ControllerPaymentPagseguro extends Controller {
+    public function index() {
+      $this->load->language('payment/mundipagg');
+
+      $data['text_instruction'] = $this->language->get('text_instruction');
+      $data['text_payable'] = $this->language->get('text_payable');
+      $data['text_address'] = $this->language->get('text_address');
+      $data['text_payment'] = $this->language->get('text_payment');
+      $data['text_loading'] = $this->language->get('text_loading');
+
+      $data['button_confirm'] = $this->language->get('button_confirm');
+
+      $data['payable'] = $this->config->get('cheque_payable');
+      $data['address'] = nl2br($this->config->get('config_address'));
+
+      $data['continue'] = $this->url->link('checkout/success');
+
+      /** Mundipagg Default functions */
+      try {
+        // Carrega dependências
+        require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
+        // Configura o ambiente da Mundi a ser usado
+        \MundiPagg\ApiClient::setEnvironment( \MundiPagg\Checkout\DataContract\Enum\ApiEnvironmentEnum::STAGING );
+        // Configura a MerchantKey a ser utilizada (Chave secreta)
+        \MundiPagg\ApiClient::setMerchantKey( "54409272-781D-4470-BFA3-C58A5A005B49" );
+        //\MundiPagg\ApiClient::setMerchantKey( "54409272-781D-4470-BFA3-C58A5A005B49" );
+        // Cria objeto de solicitação do token
+        $tokenRequest = new \MundiPagg\Checkout\DataContract\Request\TokenRequest();
+        // Define dados do pedido
+        $tokenRequest->getOrder()->setOrderReference( sprintf( "Test #%s", time() ) )->setAmountInCents( 100 );
+        // Define opções do pedido
+        $tokenRequest->getOptions()->disableAmountInCentsUpdate()->enableCreditCardPayment()->disableBoletoPayment()->disableAntiFraud();
+        //Cria um objeto ApiClient
+        $apiClient = new \MundiPagg\ApiClient();
+        // Faz a chamada para criação do token
+        $tokenResponse = $apiClient->processCheckoutRequest( $tokenRequest );
+        // Chama a view do form
+        //require 'form.php';
+
+      } catch ( \MundiPagg\Checkout\DataContract\Report\ApiError $error ) {
+        http_response_code( 500 );
+        print_r( $error );
+      } catch ( \Exception $ex ) {
+        var_dump( $ex );
+        echo $ex->getMessage();
+      }
+      /** Opencart 2.0.x Default */
+      if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/mundipagg.tpl')) {
+        return $this->load->view($this->config->get('config_template') . '/template/payment/mundipagg.tpl', $data);
+      } else {
+        return $this->load->view('default/template/payment/mundipagg.tpl', $data);
+      }
+
+    }
+  }
+
+
